c282189765f91ae365ebc146241b308264d16c42
qthais <thaiallb63@gmail.com>
2024-08-26 22:33:32 +0700

    oke

---
+++ components/CallToAction.jsx
@@ -0,0 +1,27 @@
+'use client'
+import React from 'react'
+import Button from './Button'
+import Link from 'next/link'
+import { useAuth } from '@/context/AuthContext'
+export default function CallToAction() {
+    const { currentUser } = useAuth()
+    if (currentUser) {
+        return (
+            <div className='max-w-[600px] mx-auto w-full'>
+                <Link href={'/dashboard'}>
+                    <Button dark full text="Go to Dashboard" />
+                </Link>
+            </div>
+        )
+    }
+    return (
+        <div className='grid grid-cols-2 gap-4 w-fit mx-auto'>
+            <Link href={'/dashboard'}>
+                <Button text="Sign Up" />
+            </Link>
+            <Link href={'/dashboard'}>
+                <Button text="Login" dark />
+            </Link>
+        </div>
+    )
+}
---
+++ components/Logout.jsx
@@ -0,0 +1,23 @@
+'use client'
+import React from 'react'
+import Button from './Button'
+import { useAuth } from '@/context/AuthContext'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+export default function Logout() {
+    const { logout,currentUser } = useAuth()
+    const pathname=usePathname()
+    if(!currentUser){
+        return null
+    }
+    if(pathname==='/'){
+        return(
+            <Link href={'/dashboard'}>
+                <Button text='Go to Dashboad'/>
+            </Link>
+        )
+    }
+    return (
+        <Button text='Logout' clickHandler={logout} />
+    )
+}
